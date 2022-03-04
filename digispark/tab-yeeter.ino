4057fce9780d03a8c14ad872abd199ab62a468a7
Edvin Källström <84442052+Eken-beep@users.noreply.github.com>
2022-03-04 09:56:56 +0100

    Update tab-yeeter.ino

--- digispark/tab-yeeter.ino
+++ digispark/tab-yeeter.ino
@@ -1,1 +1,11 @@
+#include <DigiKeyboard.h>
 
+void setup() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, KEY_W);
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, KEY_W);
+  DigiKeyboard.delay(100);
+}
